ad5f0b0aa25dd188ba65c7c1f3ac1b89184d8333
Leandro Rey <leandrodrey@gmail.com>
2023-04-20 02:08:14 -0300

    Moved some html code from template (index) to App.jsx and perform some css changes

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -35,6 +35,22 @@ function App() {
                     menuItems={menuItems}
                 />
             </header>
+
+            <div id="index-banner" className="parallax-container">
+                <div className="section no-pad-bot">
+                    <div className="container">
+                        <br/><br/>
+                        <h1 className="header center teal-text text-lighten-2">Parallax Template</h1>
+                        <div className="row center">
+                            <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                        </div>
+                        <div className="row center">
+                            <a href="http://materializecss.com/getting-started.html" id="download-button" className="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+                        </div>
+                        <br/><br/>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
