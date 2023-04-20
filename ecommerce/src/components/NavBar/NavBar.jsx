1ae24f0e99023695564b247fb1a1882bdd732c6f
Leandro Rey <leandrodrey@gmail.com>
2023-04-20 00:06:46 -0300

    NavBar component created, first approach

---
+++ ecommerce/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {CartButton} from "./CartButton";
+
+const NavBar = () => {
+    return (
+        <div>
+            <CartButton />
+        </div>
+    )
+}
+export default NavBar
