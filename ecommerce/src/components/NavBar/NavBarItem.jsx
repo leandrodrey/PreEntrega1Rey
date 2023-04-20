9717cd0c17c6b541dae7e20e72b79aa3b8496240
Leandro Rey <leandrodrey@gmail.com>
2023-04-20 01:41:04 -0300

    NavBar item component improved

--- ecommerce/src/components/NavBar/NavBarItem.jsx
+++ ecommerce/src/components/NavBar/NavBarItem.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const NavBarItem = (props) => {
+
+    const linkTitle = props.text;
+    const linkText = props.text;
+    const linkUrl = props.link;
+
     return (
-        <a href={props.link}>
-            {props.text}
+        <a title={linkTitle} href={linkUrl}>
+            {linkText}
         </a>
     )
 }
