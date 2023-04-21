91799f02e7ca92c0b2b6783745b4c9c2334ca7a6
Leandro Rey <leandrodrey@gmail.com>
2023-04-21 20:10:23 -0300

    CartWidget import updated

--- ecommerce/src/components/NavBar/NavBar.jsx
+++ ecommerce/src/components/NavBar/NavBar.jsx
@@ -11,7 +11,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import {CartWidget} from "./CartWidget";
+import CartWidget from "./CartWidget/CartWidget";
 
 function NavBar(props) {
 
