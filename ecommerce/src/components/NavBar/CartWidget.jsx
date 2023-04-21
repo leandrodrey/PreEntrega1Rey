0bb00b349c7aaf2fa10509cb89b1145bd85baa77
Leandro Rey <leandrodrey@gmail.com>
2023-04-21 18:48:31 -0300

    Cart Widget updated

--- ecommerce/src/components/NavBar/CartWidget.jsx
+++ ecommerce/src/components/NavBar/CartWidget.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import {IconButton} from "@mui/material";
+import * as React from 'react';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        right: -3,
+        top: 13,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px'
+    },
+}));
 
 export const CartWidget = () => {
     return (
         <React.Fragment>
-            <IconButton color="primary" aria-label="shopping cart button">
-                <ShoppingCartIcon /> <span>5</span>
+            <IconButton color="terciary" aria-label="shopping cart button">
+                <StyledBadge badgeContent={4} max={999} color="secondary">
+                    <ShoppingCartIcon />
+                </StyledBadge>
             </IconButton>
         </React.Fragment>
     )
