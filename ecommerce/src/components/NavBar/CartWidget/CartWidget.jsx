d0385d27b372da0ad5e14dc846930299ba470622
Leandro Rey <leandrodrey@gmail.com>
2023-04-21 20:12:14 -0300

    Code Style

--- ecommerce/src/components/NavBar/CartWidget/CartWidget.jsx
+++ ecommerce/src/components/NavBar/CartWidget/CartWidget.jsx
@@ -8,7 +8,7 @@ const CartWidget = () => {
     return (
         <React.Fragment>
             <IconButton aria-label="shopping cart button">
-                <Badge  className="shopping-cart__badge" badgeContent={4} max={999} color="secondary">
+                <Badge className="shopping-cart__badge" badgeContent={4} max={999} color="secondary">
                     <ShoppingCartIcon className="shopping-cart__icon" fontSize="medium" />
                 </Badge>
             </IconButton>
