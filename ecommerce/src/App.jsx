9f3fba21c791410266a79a3f09e52dd82c88f9ec
Leandro Rey <leandrodrey@gmail.com>
2023-04-22 12:11:50 -0300

    Changed to Dark Theme

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -5,38 +5,48 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { StyledEngineProvider } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import NavBar from "./components/NavBar/NavBar";
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+const menuItems = [
+    {
+        name: 'Home',
+        link: 'text.com'
+    },
+    {
+        name: 'Store',
+        link: 'text.com'
+    },
+    {
+        name: 'About Us',
+        link: 'text.com'
+    },
+    {
+        name: 'Contact Us',
+        link: 'text.com'
+    }
+];
+
 function App() {
-
-    const menuItems = [
-            {
-                name: 'Home',
-                link: 'text.com'
-            },
-            {
-                name: 'Store',
-                link: 'text.com'
-            },
-            {
-                name: 'About Us',
-                link: 'text.com'
-            },
-            {
-                name: 'Contact Us',
-                link: 'text.com'
-            }
-        ];
-
     return (
         <StyledEngineProvider injectFirst>
-            <header>
-                <NavBar
-                    title="Overlord Store"
-                    logo="Overlord Store" //TODO: Set a logo image
-                    menuItems={menuItems}
-                />
-            </header>
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
+                <header>
+                    <NavBar
+                        title="Overlord Store"
+                        logo="Overlord Store" //TODO: Set a logo image
+                        menuItems={menuItems}
+                    />
+                </header>
+            </ThemeProvider>
         </StyledEngineProvider>
     );
 }
